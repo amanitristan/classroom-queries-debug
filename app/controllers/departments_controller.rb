816f3095a4f9712bae58027a4d495abd36d298d8
Amani T <136659501+amanitristan@users.noreply.github.com>
2023-08-18 03:33:40 +0000

    Fixed code for departments routes

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -7,7 +7,8 @@ class DepartmentsController < ApplicationController
 
   def show
     the_id = params.fetch("path_id")
-    @department = Department.where({:id => the_id })
+    department = Department.where({:id => the_id })
+    @the_dept = department.at(0)
 
     render({ :template => "departments/show" })
   end
